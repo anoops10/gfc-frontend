e5388fc9e4380056a9aa3bab9cb5f2d5d4054b6a
anoops10 <anoopshekhar85@gmail.com>
2026-03-21 02:09:09 +0530

    Update EmailService.jsx with new sendMail function

--- src/services/EmailService.jsx
+++ src/services/EmailService.jsx
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-
 export async function sendMail(mail) {
     try {
-        const response = await axios.post(`http://localhost:8080/sendmail`, mail);
+        const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/sendmail`, mail);
         return response.data;
     } catch (error) {
         console.log("error sending mail", error.response?.data || error.message);
